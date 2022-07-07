97b3bce0fc52f2d6b9e7b3e46896d41d354e867e
Max Taylor <maxbransontaylor@gmail.com>
2022-07-06 21:44:58 -0400

    get and display issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,29 +1,38 @@
+var issueContainerEl = document.querySelector("#issues-container");
 var getRepoIssues = function (repo) {
-    var url = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(url).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                displayIssues(data);
-            });
-        } else { alert("There was a problem with your request.") }
-    });
+  var url = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+  fetch(url).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayIssues(data);
+      });
+    } else {
+      alert("There was a problem with your request.");
+    }
+  });
 };
 var displayIssues = function (issues) {
-    for (var i = 0; i < data.length; i++) {
-        var issueEl = document.createElement("a");
-        issueEl.classList = "list-item flex-row justify-space-between align-center";
-        issueEl.setAttribute("href", issues[i].html_url);
-        issueEl.setAttribute("target", "_blank");
-        var titleEl = document.createElement("span");
-        titleEl.textContent = issues[i].title;
-        issueEl.appendChild(titleEl);
-        var typeEl = document.createElement("span");
-        if (issues[i].pull_request) {
-            typeEl.textContent = "(Pull request)";
-        } else { typeEl.textContent = "(Issue)" }
+  if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues!";
+    return;
+  }
+  for (var i = 0; i < issues.length; i++) {
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+    var titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
+    issueEl.appendChild(titleEl);
+    var typeEl = document.createElement("span");
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull request)";
+    } else {
+      typeEl.textContent = "(Issue)";
     }
     issueEl.appendChild(typeEl);
+    issueContainerEl.appendChild(issueEl);
+  }
 };
 
-getRepoIssues("facebook/react")
-//6.3.5
+getRepoIssues("facebook/react");
